3d3a044ad2c2f38603c62dc086084df13a9cc14a
Suheldevs <mohdsuhel.dev@gmail.com>
2025-08-20 15:44:30 +0530

    config changes

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -1,18 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
-    typescript: {
-        ignoreBuildErrors: true,
-        
-    },
-    // distDir: 'dist',
-    trailingSlash: true,
-    images: { unoptimized: true },
-    reactStrictMode: false,  
-    experimental: {
-        missingSuspenseWithCSRBailout: false,
-      },
-
+  reactStrictMode: false,
+  trailingSlash: true,
+  images: { unoptimized: true },
+  typescript: { ignoreBuildErrors: true },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  // output: 'export' // remove this for dynamic support
 }
 
 module.exports = nextConfig
